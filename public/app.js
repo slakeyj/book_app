a72cc16a6719a345d530132d2f10bbc0b2733695
Susanna Lakey <susannalakey@gmail.com>
2019-08-30 21:03:20 -0700

    fixed bug with the form toggle

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function () {
-  $('#show-form-button"').click(function (event) {
+  $('#show-form-button').click(function (event) {
     event.preventDefault();
     $('.toggle-value').attr('type', 'text');
     $('#save-data').toggle();
-    $('#show-form-button"').toggle()
+    $('#show-form-button').toggle()
   });
 });
